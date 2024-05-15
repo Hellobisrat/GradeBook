d174bfcf08022c57da3537d3501f945d0ab0992f
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-05-14 22:14:29 -0500

    script part added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,44 @@
 var student = document.getElementById("student-names")
 var grade = document.getElementById('grades')
-var comment = 
+var comment = document.getElementById('msg')
+var saveButton = document.getElementById('save')
+var saveName = document.getElementById('save-name')
+
+
+function saveLastGrade() {
+  var studentGrade = {
+    student: student.value,
+    grade:grade.value,
+    comment:comment.value.trim()
+  }
+
+  localStorage.setItem('studentGrade',JSON.stringify(student))
+}
+
+function renderLastGrade() {
+  var lastGrade = JSON.parse(localStorage.getItem)
+
+  if(lastGrade !== null) {
+    document.getElementById('saved-name').innerHTML =lastGrade.student
+    document.getElementById('saved-grade').innerHTML=lastGrade.innerHTML
+    document.getElementById('save-comment').innerHTML=lastGrade.comment
+  }
+  else
+  {
+    return;
+  }
+} 
+
+
+saveButton.addEventListener('click',function(event){
+  event.preventDefault();
+  saveLastGrade();
+  renderLastGrade()
+});
+
+function init() {
+  renderLastGrade()
+
+}
+
+init()
